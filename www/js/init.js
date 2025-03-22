6ae28bda998c196bce1c24450d042fda83e95e67
z3phyrl <nonamefound65@gmail.com>
2025-03-23 03:51:36 +0700

    Use new LibService and Page library

---
+++ www/js/init.js
@@ -0,0 +1,31 @@
+let zeitop = null;
+
+let body = document.body;
+let head = document.head;
+
+let app = document.getElementById("app");
+
+function load_lib(name, callback) {
+    callback = callback || (() => {});
+    zeitop.request("lib", name, (lib) => {
+        let script = document.createElement("script");
+        script.innerHTML = lib;
+        document.body.appendChild(script);
+        callback();
+    }, "");
+}
+
+function onready(ws) {
+    load_lib("page", () => {
+        request_page("default", (page) => {
+            page.apply_style();
+            page.append_to(app);
+        });
+    });
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    zeitop = new Zeitop(device.serial, 6969, onready);
+}
+
--- www/js/zeitop.js
+++ www/js/zeitop.js
@@ -24,23 +24,23 @@ class Zeitop {
                 this.ws.send("?");
                 return;
             }
-            if (msg.data.startsWith("!")) {
-                log(msg.data);
-                return;
-            }
             let data = msg.data;
             let at_index = data.indexOf("@");
             let tag_index = data.indexOf("#");
             if (tag_index > at_index || tag_index < 0) {tag_index = at_index}
             let data_index = data.indexOf("::", at_index);
-            let parse = [data.slice(0, tag_index), data.slice(tag_index + 1, at_index), data.slice(at_index + 1, data_index), data.slice(data_index + 2)];
+            let parse = [data.slice(0, tag_index), data.slice(tag_index, at_index), data.slice(at_index + 1, data_index), data.slice(data_index + 2)];
             let request = parse[0];
             let tag = parse[1];
             let service = parse[2];
             let reply = parse[3];
+            if (reply.startsWith("!")) {
+                log(reply);
+                return;
+            }
             let callback = this.requests.get(service);
             if (callback == null) {
-                callback = this.requests.get(service + "::" + request);
+                callback = this.requests.get(service + "::" + request + tag);
             }
             callback(reply);
         }
@@ -79,7 +79,7 @@ class Zeitop {
                 this.requests.delete((service + "::" + request))
                 callback(reply)
             }
-            this.requests.set((service + "::" + request), remove_on_callback)
+            this.requests.set((service + "::" + request + tag), remove_on_callback)
             this.ws.send("&" + service + tag + "::" + request)
             return true;
         }
